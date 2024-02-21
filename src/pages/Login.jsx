fc7563f3f72a23fdfa5d3d3afac61d77eac079f2
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-02-21 18:43:29 +0530

    fixed login issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { login } from '../store/authSlice'
-import { toast }  from 'react-toastify'
+import { toast } from 'react-toastify'
 import LoginComp from '../components/Login/LoginComp'
 
 function Login() {
@@ -14,35 +14,39 @@ function Login() {
   const handelSubmit = async (e) => {
     e.preventDefault()
 
-    await toast.promise(
+    await
       axios
-          .post('https://dummyjson.com/auth/login', {
-              username: e.target.userName.value,
-              password: e.target.password.value,
-              expiresInMins: 60
-          })
-          .then(response => {
-            localStorage.setItem("authToken", response.data.token);
+        .post('https://dummyjson.com/auth/login', {
+          username: e.target.userName.value,
+          password: e.target.password.value,
+          expiresInMins: 60
+        })
+        .then(response => {
+          localStorage.setItem("authToken", response.data.token);
 
-            axios
+          axios
             .get('https://dummyjson.com/auth/me', { headers: { 'Authorization': `Bearer ${response.data.token}` } })
             .then(response => {
-                dispatch(login(response.data.token))
-                navigate("/")
+              dispatch(login(response.data.token))
+              toast.success('Logged In 👍', {
+                progress: false,
+              },)
+              navigate("/")
             })
             .catch(error => {
-                console.log(error);
+              // console.log(error.message)
+              toast.error(`Login Failed : ${error.message} 🤯`, {
+                progress: false,
+              })
             })
+        })
+        .catch((error) => {
+          // console.log(error.message);
+          toast.error(`Login Failed : ${error.message} 🤯`, {
+            progress: false,
           })
-          .catch((error) => {
-            console.log(error);
-          }),
-          {
-            pending: 'Logging In...',
-            success: 'Logged In 👍',
-            error: 'Login Failed 🤯',
-          }
-  )}
+        })
+  }
 
   return (
     <LoginComp handelSubmit={handelSubmit} />
