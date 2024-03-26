bc568c22807f219448026e7bf2ad23029769512d
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-03-27 03:26:51 +0530

    fixed deployment bug

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import ProductPageComp from '../components/ProductPageComp'
 import { useParams } from 'react-router-dom'
 import useFetch from "../hooks/useFetch"
 import { lazy, Suspense } from 'react'
