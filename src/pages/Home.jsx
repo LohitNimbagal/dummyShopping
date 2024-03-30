9edd2bf848c45b07233d6d781e81532ed5e29bbb
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-03-30 17:41:10 +0530

    updated folder namings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import useFetch from '../hooks/useFetch'
+import Card from '../components/ui/Card';
 import { Loading } from '../components/Loading';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCart, removeFromCart } from '../store/cartThunkSlice'
